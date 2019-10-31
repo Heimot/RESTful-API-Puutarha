285a95eb5f0ed328c74163b091062e90e2b9ecaa
Heimot <heimonenjoonas@gmail.com>
2019-10-31 13:16:27 +0200

    Added user signup and bcrypt

---
+++ api/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    email: {
+        type: String,
+        required: true,
+        unique: true/*remove comment and comment marks if i want to use email and not username (this validates that the email looks right) :),
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/*/
+    },
+    password: { type: String, required: true }
+});
+
+module.exports = mongoose.model('User', userSchema);
--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 
 mongoose.connect('mongodb+srv://Heimot:'+ process.env.MONGOPW +'@node-rest-api-8ybrw.mongodb.net/test?retryWrites=true&w=majority',
     {
@@ -33,6 +34,7 @@ app.use((req, res, next) => {
 // Routes which handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes)
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
