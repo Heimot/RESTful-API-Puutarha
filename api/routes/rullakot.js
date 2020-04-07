f8d0e33ab395b9a47cbf2004334d9d62b53dc1d7
Heimot <heimonenjoonas@gmail.com>
2020-04-07 12:58:17 +0300

    Name change.

--- api/routes/rullakot.js
+++ api/routes/rullakot.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const checkAuth = require('../middleware/check-auth');
-const RullakotController = require('../controllers/rullakko');
+const RullakotController = require('../controllers/rullakot');
 
 router.get('/rullakot', checkAuth, RullakotController.rullakko_get_all);
 
