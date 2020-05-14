8f8c097a76be3a62ff1de7d4b95e698072722997
Heimot <heimonenjoonas@gmail.com>
2020-05-14 12:30:48 +0300

    Added position

--- api/models/order.js
+++ api/models/order.js
@@ -7,15 +7,17 @@ const orderSchema = mongoose.Schema({
     alisatieto: { type: String, required: false },
     date: { type: String, required: true },
     toimituspvm: { type: String, required: true },
-    tuusjarvi: { type: String, required: true, default: "Ei"},
-    ryona: { type: String, required: true, default: "Ei"},
+    tuusjarvi: { type: String, required: true, default: "Ei" },
+    ryona: { type: String, required: true, default: "Ei" },
 
-    products: [{type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: false}],
-    
-    rullakot: [{type: mongoose.Schema.Types.ObjectId, ref: 'Rullakot', required: false}],
-    hyllyt: [{type: mongoose.Schema.Types.ObjectId, ref: 'Hyllyt', required: false}],
+    products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: false }],
 
-    rekka: {type: String, required: true, default: "Ei valittu"}
+    rullakot: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Rullakot', required: false }],
+    hyllyt: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Hyllyt', required: false }],
+
+    rekka: { type: String, required: true, default: "Ei valittu" },
+
+    position: { type: Number, required: true, default: 0 }
 });
 
 module.exports = mongoose.model('Order', orderSchema);
